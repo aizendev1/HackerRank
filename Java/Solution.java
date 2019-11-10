17589cb5d3947b8f70837c28db3921d642e0b920
Aizen <aizendev1@gmail.com>
2019-11-10 12:58:02 +0200

    Work

---
+++ Java/Solution.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String ln;
+        int i = 1;
+        while(input.hasNext())
+        {
+            ln = input.nextLine();
+            System.out.println(i + " "+ ln);
+            i++;
+        }
+    }
+}
+
+
